182c4935ceefb9d9b866d1573ba6ba77d434e33f
bhanu <bhanu@foodmonk.com>
2020-04-07 23:48:12 +0530

    minor fix

--- turn/main.go
+++ turn/main.go
@@ -15,9 +15,52 @@ import (
 
 func createAuthHandler() turn.AuthHandler {
 	return func(username string,  realm string, srcAddr net.Addr) (key []byte, ok bool) {
-		return "password", true
+		return []byte("password"), true
 	}
 }
+
+// RelayAddressGenerator is used to generate a RelayAddress when creating an allocation.
+// You can use one of the provided ones or provide your own.
+type RelayAddressGenerator interface {
+	// Validate confirms that the RelayAddressGenerator is properly initialized
+	Validate() error
+
+	// Allocate a PacketConn (UDP) RelayAddress
+	AllocatePacketConn(network string, requestedPort int) (net.PacketConn, net.Addr, error)
+
+	// Allocate a Conn (TCP) RelayAddress
+	AllocateConn(network string, requestedPort int) (net.Conn, net.Addr, error)
+}
+
+type PacketConnConfig struct {
+	PacketConn net.PacketConn
+
+	// When an allocation is generated the RelayAddressGenerator
+	// creates the net.PacketConn and returns the IP/Port it is available at
+	RelayAddressGenerator RelayAddressGenerator
+}
+
+// ListenerConfig is a single net.Listener to accept connections on. This will be used for TCP, TLS and DTLS listeners
+type ListenerConfig struct {
+	Listener net.Listener
+
+	// When an allocation is generated the RelayAddressGenerator
+	// creates the net.PacketConn and returns the IP/Port it is available at
+	RelayAddressGenerator RelayAddressGenerator
+}
+
+func (c *ListenerConfig) validate() error {
+	if c.Listener == nil {
+		return errListenerUnset
+	}
+
+	if c.RelayAddressGenerator == nil {
+		return errRelayAddressGeneratorUnset
+	}
+
+	return c.RelayAddressGenerator.Validate()
+}
+
 
 func main() {
 	sigs := make(chan os.Signal, 1)
@@ -53,7 +96,7 @@ func main() {
 		}
 	}
 
-	s := turn.NewServer(&turn.ServerConfig{
+	s := turn.NewServer(turn.ServerConfig{
 		Realm:              realm,
 		AuthHandler:        createAuthHandler(),
 		ChannelBindTimeout: channelBindTimeout,
